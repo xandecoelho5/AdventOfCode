420ffb2d4415fe3350f85620a146df76bb6e9994
xandecoelho5 <xandecoelho5@hotmail.com>
2023-09-12 02:30:58 -0300

    removed useless fun

--- 2021/kotlin/src/main/kotlin/Utils.kt
+++ 2021/kotlin/src/main/kotlin/Utils.kt
@@ -4,8 +4,3 @@ import java.io.File
  * Reads lines from the given input txt file.
  */
 fun readInput(name: String) = File("src/jetbrains.main/resources", "$name.txt").readLines()
-
-/**
- * The cleaner shorthand for printing output.
- */
-fun Any?.println() = println(this)
